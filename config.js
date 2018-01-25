00e755bdb559102247b8c94cf6ec35b266ecabce
Ambrose Chua <ambrose@chua.family>
2018-01-25 23:20:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	oidc: {
-		issuer: "https://login.nushigh.edu.sg/sso",
-		client_id: "thepool",
-		client_secret: "812Ri48Zqp1lrbWoaHZmszi8TCEH0D8k",
+		issuer: process.env.ISSUER || "https://login.nushigh.edu.sg/sso",
+		client_id: process.env.CLIENT_ID || "thepool",
+		client_secret: process.env.CLIENT_SECRET || "812Ri48Zqp1lrbWoaHZmszi8TCEH0D8k",
 		redirect_uri: process.env.REDIRECT_URI || "http://commongoods.nushigh.edu.sg/callback",
 	},
 	allowed: process.env.DEBUG ? /(nhs.+@nus\.edu\.sg|h1310031@nushigh\.edu\.sg|h1210108@nushigh\.edu\.sg|appventure@nushigh.edu.sg)/ : /(nhs.+@nus\.edu\.sg)/
