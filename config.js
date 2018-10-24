a27e6afc789a63137d214df6f95d05439676e631
Ambrose Chua <h1310031@nushigh.edu.sg>
2018-10-24 08:48:25 +0800

    Switch teachers to NUS High domain

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ module.exports = {
 		client_secret: process.env.CLIENT_SECRET || "812Ri48Zqp1lrbWoaHZmszi8TCEH0D8k",
 		redirect_uri: process.env.REDIRECT_URI || "http://commongoods.nushigh.edu.sg/callback",
 	},
-	allowed: process.env.DEBUG ? /(nhs.+@nus\.edu\.sg|.+@nushigh\.edu\.sg)/ : /(nhs.+@nus\.edu\.sg)/
+	allowed: process.env.DEBUG ? /(nhs.+@nus\.edu\.sg|.+@nushigh\.edu\.sg)/ : /(nhs.+@nus\.edu\.sg|nhs.+@nushigh.edu.sg)/
 };
